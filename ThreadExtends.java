8d0c205a0a88bbbdf815c37c1c6dc3bd6e37d16c
Srinivas <73590663+Srinivaskasani97@users.noreply.github.com>
2020-11-18 12:41:13 +0530

    Add files via upload

---
+++ ThreadExtends.java
@@ -0,0 +1,16 @@
+public class ThreadExtends extends Thread {
+
+	public void run() {
+		for (int i = 0; i <= 20; i++) {
+			System.out.println("i value: " + i);
+		}
+	}
+
+	public static void main(String[] args) {
+		ThreadExtends te = new ThreadExtends();
+		te.start();
+		for (int j = 0; j <= 20; j++) {
+			System.out.println("j value: " + j);
+		}
+	}
+}
---
+++ ThreadRunnable.java
@@ -0,0 +1,19 @@
+public class ThreadRunnable implements Runnable {
+
+	public static void main(String[] args) {
+		ThreadRunnable tr = new ThreadRunnable();
+		Thread t = new Thread(tr);
+		t.start();
+
+		for (int j = 0; j <= 20; j++) {
+			System.out.println("j value: " +j);
+		}
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i <= 20; i++) {
+			System.out.println("i value: " + i);
+		}
+	}
+}
